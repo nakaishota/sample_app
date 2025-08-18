90a45277ddd6550bfaa61308ede28c6fd367ecb0
nakaishota <nakaishota8@gmail.com>
2025-08-18 12:44:30 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not 'div.alert-success'.empty?
+    assert is_logged_in?
   end
 end
